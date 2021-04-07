c7f5e3ba4abe58a009ec779b0a68fdb0c3b284cf
Cameron Reikes <cameronreikes@gmail.com>
2021-04-07 15:26:34 -0700

    Drawing and creating multiple lines

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Point;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 use std::time::Duration;
+
+trait Drawable {
+    fn draw(&self, canvas: &mut Canvas<sdl2::video::Window>);
+    fn process_event(&mut self, e: &Event);
+}
+
+struct Line {
+    points: Vec<Point>,
+}
+
+impl Line {
+    fn new() -> Self {
+        Line { points: Vec::new() }
+    }
+}
+
+impl Drawable for Line {
+    fn draw(&self, canvas: &mut Canvas<Window>) {
+        canvas.set_draw_color(Color::BLACK);
+        canvas.draw_lines(&self.points[..]).unwrap();
+    }
+    fn process_event(&mut self, e: &Event) {
+        if let Event::MouseMotion { x, y, .. } = *e {
+            self.points.push(Point::new(x, y));
+        }
+    }
+}
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -12,32 +42,50 @@ pub fn main() {
     let window = video_subsystem
         .window("explain", 800, 600)
         .position_centered()
+        .resizable()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+    let mut items: Vec<Box<dyn Drawable>> = Vec::new();
+    let mut item_currently_creating: Option<Box<dyn Drawable>> = None;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.set_draw_color(Color::WHITE);
         canvas.clear();
+
         for event in event_pump.poll_iter() {
+            if let Some(item) = &mut item_currently_creating {
+                item.process_event(&event);
+            }
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
+                // TODO check the mouse_btn to see if it's left click, and check the tool that is
+                // active will create stuff (tool might be trait too)
+                Event::MouseButtonDown { mouse_btn, .. } => {
+                    item_currently_creating = Some(Box::new(Line::new()));
+                }
+                Event::MouseButtonUp { mouse_btn, .. } => {
+                    if item_currently_creating.is_some() {
+                        items.push(item_currently_creating.unwrap());
+                        item_currently_creating = None;
+                    }
+                }
                 _ => {}
             }
         }
 
-        // The rest of the game loop goes here...
+        for i in items.iter() {
+            i.draw(&mut canvas);
+        }
+        if let Some(item) = &item_currently_creating {
+            item.draw(&mut canvas);
+        }
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
